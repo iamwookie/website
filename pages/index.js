a572b12a75d58fc8a63906507eaf4ffa07741d9f
Wookie <bq1214@gmail.com>
2022-09-28 21:06:05 +0500

    style: changed margin on avatar

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home({ discordTag, avatarURL }) {
                 <div className='backdrop-blur-xl p-5 rounded-lg text-center text-white font-nunito animate__animated animate__fadeIn'>
                     <Image src={avatarURL} width={100} height={100} alt='Avatar' className='mx-auto w-36 rounded-full border-2' />
 
-                    <h1 className='mt-1 text-2xl sm:text'>{"Hi, I'm Bilal"}</h1>
+                    <h1 className='mt-2 text-2xl sm:text'>{"Hi, I'm Bilal"}</h1>
                     <h2 className='mt-1 text-sm'>Code | Media | Design</h2>
 
                     <p className='mt-4 py-4 border-y-2 text-base'>
