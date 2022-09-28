23de0952db749b5e4ce3073c3929c404bafb55c9
Wookie <bq1214@gmail.com>
2022-09-28 20:48:10 +0500

    refactor: changed variable name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Discord from '../libs/discord';
 
-export default function Home({ discordTag, profileURL }) {
+export default function Home({ discordTag, avatarURL }) {
     const [musicData, setMusic] = useState(null);
 
     useEffect(() => {
@@ -36,7 +36,7 @@ export default function Home({ discordTag, profileURL }) {
 
             <main className='grid min-h-screen place-items-center'>
                 <div className='backdrop-blur-xl p-5 rounded-lg text-center text-white font-nunito animate__animated animate__fadeIn'>
-                    <Image src={profileURL} width={100} height={100} alt='Avatar' className='mx-auto w-36 rounded-full border-2' />
+                    <Image src={avatarURL} width={100} height={100} alt='Avatar' className='mx-auto w-36 rounded-full border-2' />
 
                     <h1 className='mt-1 text-2xl sm:text'>{"Hi, I'm Bilal"}</h1>
                     <h2 className='mt-1 text-sm'>Code | Media | Design</h2>
@@ -87,7 +87,7 @@ export async function getServerSideProps() {
     return {
         props: {
             discordTag: user.tag,
-            profileURL: user.avatarURL,
+            avatarURL: user.avatarURL,
         }
     };
 }
