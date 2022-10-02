c97bbde2ef5e24caf755ba8f624cb3121cf18b4a
Wookie <bq1214@gmail.com>
2022-10-02 15:26:42 +0500

    style: changed description and titles

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,10 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <meta name="author" content="Bilal" />
                 <meta name="theme-color" content="#FFFFFF" />
-                <meta name="description" content="Hi, I'm Bilal." />
+                <meta name="description" content="A person on this planet." />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="( ͡° ͜ʖ ͡°)" key='title' />
-                <meta property="og:description" content="Hi, I'm Bilal." key='desc' />
+                <meta property="og:description" content="A person on this planet." key='desc' />
                 <meta property="og:url" content="https://bil.al" />
                 <meta property="og:image" content="/assets/favicon.png" />
                 <link rel="icon" href="/assets/favicon.png" />
