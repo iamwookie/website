28a2b2b93e0663e9043acd487cddbd9eaf56725d
Wookie <bq1214@gmail.com>
2023-01-31 01:06:48 +0500

    style: changed styling for KAT page

--- pages/kat.tsx
+++ pages/kat.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/future/image";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
@@ -16,52 +17,61 @@ type Props = {
 
 export default function KAT({ avatarURL, stats }: Props) {
     return (
-        <Layout>
-            {avatarURL && (
-                <Image
-                    src={avatarURL}
-                    width={100}
-                    height={100}
-                    alt="Avatar"
-                    className="mx-auto w-36 rounded-full border-2"
-                />
-            )}
+        <>
+            <Head>
+                <title>KAT</title>
+                <meta name="description" content="✨ A multi-purpose Discord bot providing solutions to most problems." />
+                <meta property="og:title" content="KAT" key="title" />
+                <meta property="og:description" content="✨ A multi-purpose Discord bot providing solutions to most problems." key="desc" />
+            </Head>
 
-            <h1 className="pt-2 text-2xl">KAT</h1>
+            <Layout>
+                {avatarURL && (
+                    <Image
+                        src={avatarURL}
+                        width={100}
+                        height={100}
+                        alt="Avatar"
+                        className="mx-auto w-36 rounded-full border-2"
+                    />
+                )}
 
-            <div className="pt-2 pb-4">
-                <Tippy
-                    content={`Currently serving ${stats?.guilds} servers with ${stats?.users} users.`}
-                    theme="blur"
-                    disabled={stats ? false : true}
-                >
-                    <span
-                        className={`cursor-default rounded-lg my-2 mx-auto px-2 border-2 ${
-                            stats
-                                ? "text-green-500 border-green-500"
-                                : "text-red-500 border-red-500"
-                        }`}
+                <h1 className="pt-2 text-2xl">KAT</h1>
+
+                <div className="pt-2 pb-4">
+                    <Tippy
+                        content={`Currently serving ${stats?.guilds} servers with ${stats?.users} users.`}
+                        theme="blur"
+                        disabled={stats ? false : true}
                     >
-                        ● {stats ? "Online" : "Offline"}
-                    </span>
-                </Tippy>
-            </div>
+                        <span
+                            className={`cursor-default rounded-lg my-2 mx-auto px-2 border-2 ${
+                                stats
+                                    ? "text-green-500 border-green-500"
+                                    : "text-red-500 border-red-500"
+                            }`}
+                        >
+                            ● {stats ? "Online" : "Offline"}
+                        </span>
+                    </Tippy>
+                </div>
 
-            <p className="py-4 border-y-2 text-sm">
-                ✨ A multi-purpose bot providing solutions to most problems.
-                <br />
-                🎵 KAT plays high quality music from YouTube and Spotify.
-                <br />
-            </p>
+                <p className="py-4 border-y-2 text-sm">
+                    ✨ A multi-purpose Discord bot providing solutions to most problems.
+                    <br />
+                    🎵 KAT plays high quality music from YouTube and Spotify.
+                    <br />
+                </p>
 
-            <div className="pt-2">
-                <Button>
-                    <a href="/kat/invite" target="_blank">
-                        Invite
-                    </a>
-                </Button>
-            </div>
-        </Layout>
+                <div className="pt-2">
+                    <Button>
+                        <a href="/kat/invite" target="_blank">
+                            Invite
+                        </a>
+                    </Button>
+                </div>
+            </Layout>
+        </>
     );
 }
 
