4f7a4200a474645766a6a4b6a14fbfe40577ae61
Wookie <bq1214@gmail.com>
2023-02-02 01:25:39 +0500

    feat(kat): added subdomain

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+    matcher: [
+        "/",
+        "/([^/.]*)" // Exclude /public files by matching all paths except for paths containing . (e.g. /logo.png)
+    ]
+}
+
+export default (req: NextRequest, res: NextResponse) => {
+    const url = req.nextUrl;
+    const host = req.headers.get("host");
+    const sub = host.split(".")[0];
+    
+    if (sub == "kat") url.pathname = `/kat${url.pathname}`;
+
+    return NextResponse.rewrite(url);
+};
