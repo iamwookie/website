a3e3809af88890c7f380fcbb30064eb5bc6d0b5f
Wookie <bq1214@gmail.com>
2023-02-02 01:26:04 +0500

    refactor: code refactor

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,15 @@ import type { NextRequest } from "next/server";
 export const config = {
     matcher: [
         "/",
-        "/([^/.]*)" // Exclude /public files by matching all paths except for paths containing . (e.g. /logo.png)
-    ]
-}
+        "/([^/.]*)", // Exclude /public files by matching all paths except for paths containing . (e.g. /logo.png)
+    ],
+};
 
 export default (req: NextRequest, res: NextResponse) => {
     const url = req.nextUrl;
     const host = req.headers.get("host");
     const sub = host.split(".")[0];
-    
+
     if (sub == "kat") url.pathname = `/kat${url.pathname}`;
 
     return NextResponse.rewrite(url);
