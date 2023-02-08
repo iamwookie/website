070b841502f8477c7bc97f1fd95cbfdc8ac2073b
Wookie <bq1214@gmail.com>
2023-02-08 21:05:07 +0500

    feat: enabled footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,39 @@
-import Image from 'next/future/image';
+import Image from "next/future/image";
+
+const link = "https://donate.unhcr.org/int/en/turkiye-syria-earthquake-emergency?utm_source=timemag&utm_medium=referral&utm_campaign=HQ_PI_EN_teq";
 
 export default function Footer() {
     return (
-        <footer className='lg:fixed md:relative w-screen bottom-4 mt-8 text-center text-white'>
-            <div className='inline-block backdrop-blur-xl rounded-lg p-2'>
-                <div className='flex items-center'>
-                    <Image src='/assets/pk_flag.svg' width={32} height={32} alt='Pakistan Flag' className='inline rounded-md' />
-                    <p className='inline ml-2 text-lg'>More than 33 million people have been affected by floods in Pakistan. <a href='https://www.islamic-relief.org.uk/pakistan-floods-appeal/' target='_blank' rel='noreferrer' className='underline hover:opacity-50'>Click here</a> to donate.</p>
+        <footer className="lg:fixed md:relative w-screen bottom-4 mt-8 text-center text-white">
+            <div className="inline-block backdrop-blur-xl rounded-lg p-2">
+                <div className="flex items-center">
+                    <Image
+                        src="/assets/tr_flag.svg"
+                        width={32}
+                        height={32}
+                        alt="Turkish Flag"
+                        className="inline rounded-md mx-1"
+                    />
+                    <Image
+                        src="/assets/sy_flag.svg"
+                        width={32}
+                        height={32}
+                        alt="Syrian Flag"
+                        className="inline rounded-md mx-1"
+                    />
+                    <p className="inline ml-1 text-lg">
+                        More than 11,000 have died due to recent earthquakes in
+                        Turkey, Syria and neighboring countries.{" "}
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="underline hover:opacity-50"
+                        >
+                            Click here
+                        </a>{" "}
+                        to donate.
+                    </p>
                 </div>
             </div>
         </footer>
