5a2be1ecde62911a1e201984023a7b05342b98b5
Wookie <bq1214@gmail.com>
2023-02-26 19:15:31 +0500

    refactor: removed unnecessary code

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
 
 export default function Box({ children }: { children: ReactNode }) {
-    return <div className="backdrop-blur-xl p-5 rounded-lg text-center text-white font-nunito animate__animated animate__fadeIn">{children}</div>
+    return <div className="backdrop-blur-xl p-5 rounded-lg text-center animate__animated animate__fadeIn">{children}</div>
 }
--- components/Main.tsx
+++ components/Main.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react";
 
 export default function Main({ children }: { children: ReactNode }) {
     return (
-        <main className="grid min-h-screen place-items-center">
+        <main className="grid min-h-screen place-items-center text-white font-nunito">
             {children}
 
             <Footer />
