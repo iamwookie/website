200648b2d17993baf116dcd9b6b36b1e5273fc8a
Wookie <bq1214@gmail.com>
2023-04-16 22:59:31 +0500

    refactor: better conditional

--- pages/api/spotify/playing.ts
+++ pages/api/spotify/playing.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Spotify from 'libs/spotify';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.headers["sec-fetch-site"] !== "same-origin") return res.status(403).end("403 Forbidden");
+    if (req.headers["sec-fetch-site"] != "same-origin") return res.status(403).end("403 Forbidden");
 
     if (req.method !== 'GET') {
         res.setHeader('Allow', 'GET');
