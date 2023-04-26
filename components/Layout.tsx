61668fe54392e13ec1614e2ee047b5e20729ee98
Wookie <bq1214@gmail.com>
2023-04-26 21:20:32 +0500

    feat: optimized background image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,17 @@
 import { ReactNode } from "react";
+import Image from "next/future/image";
 import Main from "./Main";
 import Box from "./Box";
+
+// background-image: url("/assets/bg.jpg");
+// background-size: cover;
+// background-position: center;
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <>
+            <Image alt="Background Image" src={"/assets/bg.jpg"} fill={true} priority={true} />
+
             <Main>
                 <Box>{children}</Box>
             </Main>
