d250d515d4146472564b016b27a5a0bc3de68a41
Wookie <bq1214@gmail.com>
2023-07-29 15:24:27 +0500

    chore: update portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -23,35 +23,37 @@ export default async function Portfolio() {
     const images = await getImages();
 
     return (
-        <>
-            <BackButton />
+        <main className="grid place-items-center min-h-screen">
+            <div className="relative m-5 text-center animate__animated animate__fadeIn">
+                <BackButton />
 
-            <h1 className="text-2xl">portfolio</h1>
-            <h5 className="text-sm">{'(random stuff)'}</h5>
+                <h1 className="text-2xl">portfolio</h1>
+                <h5 className="text-sm">{'(random stuff)'}</h5>
 
-            <TechStack />
+                <TechStack />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
-                {images.map((image) => {
-                    return (
-                        <PortfolioCard
-                            name={image.name}
-                            description={image.description}
-                            bannerURL={image.bannerURL}
-                            blurDataURL={image.blurDataURL}
-                            link={image.link}
-                            key={image.name}
-                        />
-                    );
-                })}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-5">
+                    {images.map((image) => {
+                        return (
+                            <PortfolioCard
+                                name={image.name}
+                                description={image.description}
+                                bannerURL={image.bannerURL}
+                                blurDataURL={image.blurDataURL}
+                                link={image.link}
+                                key={image.name}
+                            />
+                        );
+                    })}
+                </div>
+
+                <p className="mt-5">
+                    icons provided by{' '}
+                    <a href="https://icons8.com/" target="_blank" className="underline hover:opacity-50">
+                        icons8
+                    </a>
+                </p>
             </div>
-
-            <p className='mt-5'>
-                icons provided by{' '}
-                <a href="https://icons8.com/" target="_blank" className="underline hover:opacity-50">
-                    icons8
-                </a>
-            </p>
-        </>
+        </main>
     );
 }
