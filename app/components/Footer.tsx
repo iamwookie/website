e053cf37736173512177cca8ac1c9c3856f4ca08
Wookie <bq1214@gmail.com>
2023-07-30 22:28:44 +0500

    fix: responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Footer() {
     return (
-        <footer className="relative lg:fixed w-screen bottom-4 mt-8 text-center animate__animated animate__fadeIn">
+        <footer className="relative dt:fixed min-w-full bottom-4 mt-4 text-center animate__animated animate__fadeIn">
             <div className="relative mx-auto h-6 w-8">
                 <Image src="/assets/pk_flag.svg" alt="Pakistan Flag" className="object-cover rounded-sm" fill />
             </div>
