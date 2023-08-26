946ad2430b3a10bc42838d1f692dc36b029ed295
Wookie <bq1214@gmail.com>
2023-08-26 20:17:02 +0500

    feat: animate title

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,12 @@
+import Title from '@components/Title';
 import Socials from '@components/Socials';
 import Spotify from '@components/Spotify';
 import LinkButton from '@components/ui/LinkButton';
 
-export default async function Home() {
+export default function Home() {
     return (
         <div className="relative m-5 dt:m-0 text-center animate__animated animate__fadeIn">
-            <h1 className="text-7xl dt:text-9xl">bilal mustafa</h1>
+            <Title />
 
             <Socials />
 
