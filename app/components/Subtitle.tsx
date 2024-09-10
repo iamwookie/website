37ab33292519044f09798b2454eb34b88df8c83b
Wookie <bq1214@gmail.com>
2024-09-10 15:56:47 +0500

    style: update subtitle

--- app/components/Subtitle.tsx
+++ app/components/Subtitle.tsx
@@ -1,43 +1,26 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-
-const headings = [
-    'professional yapper',
-    'average gamer',
-    'internet explorer',
-    'caffeine addict',
-    'programmer',
-    'engineer',
-    'student',
-    "can't think of more",
-];
+import { useMediaQuery } from 'react-responsive';
+import { motion } from 'framer-motion';
 
 export default function Subtitle() {
-    const [index, setIndex] = useState(0);
-    const [direction, setDirection] = useState(1);
-
-    useEffect(() => {
-        setDirection((d) => -d);
-        const intervalId = setInterval(() => setIndex((i) => (i + 1) % headings.length), 2000);
-        return () => clearInterval(intervalId);
-    }, [index]);
+    const isDesktop = useMediaQuery({ minWidth: 768 });
 
     return (
-        <div className="relative bottom-2 w-full h-6 md:h-7 text-center overflow-hidden">
-            <AnimatePresence initial={false}>
-                <motion.div
-                    key={headings[index]}
-                    initial={{ y: direction == 1 ? 100 : -100, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: direction == 1 ? -100 : 100, opacity: 0 }}
-                    transition={{ type: 'spring', mass: 0.5 }}
-                    className="absolute w-full text-md md:text-xl tracking-[0.25rem] z-10"
-                >
-                    {headings[index]}
-                </motion.div>
-            </AnimatePresence>
-        </div>
+        <motion.div
+            className="relative bottom-2 w-full text-center overflow-hidden"
+            initial={{ height: 0 }}
+            animate={{ height: isDesktop ? '1.75rem' : '1.5rem' }}
+            transition={{ delay: 3 }}
+        >
+            <motion.div
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ type: 'spring', mass: 0.5, delay: 3.2 }}
+                className="absolute w-full text-md md:text-xl tracking-[0.25rem] z-10"
+            >
+                professional yapper
+            </motion.div>
+        </motion.div>
     );
 }
