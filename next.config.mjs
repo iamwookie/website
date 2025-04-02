3a5afe3f67f6d40469f830177618aa9538be0cc3
Wookie <bq1214@gmail.com>
2025-04-02 04:00:14 +0100

    chore: latest deps

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ const withAnalyzer = withBundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    },
     reactStrictMode: true,
     images: {
         remotePatterns: [
@@ -53,31 +63,6 @@ const nextConfig = {
                 ],
             },
         ];
-    },
-    webpack(config) {
-        // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
-        config.module.rules.push(
-            // Reapply the existing rule, but only for svg imports ending in ?url
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/, // *.svg?url
-            },
-            // Convert all other *.svg imports to React components
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-                use: ['@svgr/webpack'],
-            },
-        );
-
-        // Modify the file loader rule to ignore *.svg, since we have it handled now.
-        fileLoaderRule.exclude = /\.svg$/i;
-
-        return config;
     },
 };
 
