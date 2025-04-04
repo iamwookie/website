a3a667c850bc738e83bcb4429b02c0f9bd334829
Wookie <bq1214@gmail.com>
2025-04-04 16:32:39 +0100

    style: redo title

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     return (
         <main className="flex flex-col">
             <section className="flex flex-col justify-center items-center gap-6 container h-screen mx-auto">
-                <h1 className="text-4xl">something cooking soon</h1>
+                <h1 className="text-4xl">cooking soon™</h1>
                 <Socials />
             </section>
         </main>
