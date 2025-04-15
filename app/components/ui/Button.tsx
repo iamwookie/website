1b73d178cd7242c52d4f2875f49907f15e9195af
Bilal <bq1214@gmail.com>
2025-04-15 15:58:47 +0100

    feat: add contact

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// motion import for server components
+import * as motion from 'motion/react-client';
+
+export default function Button({ content }: { content?: string }) {
+    const variants = { active: { scale: 1.05, color: '#000000', backgroundColor: '#ffffff' } };
+
+    return (
+        <motion.div
+            variants={variants}
+            whileHover="active"
+            whileTap="active"
+            transition={{ duration: 0.2 }}
+            className="rounded-lg border border-white/20 px-2 text-sm"
+        >
+            {content}
+        </motion.div>
+    );
+}
