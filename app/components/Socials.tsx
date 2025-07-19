99cd5bc16d2cffd58d21688630ec11c1f465dc0d
Bilal <mail@bil.al>
2025-07-19 15:18:41 +0500

    feat: implement tooltips
    Also re-added LinkedIn social.

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -3,7 +3,9 @@
 import { motion } from 'motion/react';
 import { toast } from 'react-toastify';
 
-// import LinkedInIcon from '@public/assets/icons/linkedin.svg';
+import Tooltip from './ui/Tooltip';
+
+import LinkedInIcon from '@public/assets/icons/linkedin.svg';
 import GitHubIcon from '@public/assets/icons/github.svg';
 import VSCOIcon from '@public/assets/icons/vsco.svg';
 import SteamIcon from '@public/assets/icons/steam.svg';
@@ -34,47 +36,61 @@ export default function Socials() {
 
     return (
         <motion.ul variants={container} initial="hidden" animate="show" className="flex w-fit items-center justify-center gap-3">
-            {/* <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.linkedin} target="_blank" rel="noreferrer noopener" aria-label="LinkedIn Link">
-                    <LinkedInIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li> */}
+            <Tooltip content="LinkedIn">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.linkedin} target="_blank" rel="noreferrer noopener" aria-label="LinkedIn Link">
+                        <LinkedInIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.github} target="_blank" rel="noreferrer noopener" aria-label="GitHub Link">
-                    <GitHubIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li>
+            <Tooltip content="GitHub">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.github} target="_blank" rel="noreferrer noopener" aria-label="GitHub Link">
+                        <GitHubIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.vsco} target="_blank" rel="noreferrer noopener" aria-label="VSCO Link">
-                    <VSCOIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li>
+            <Tooltip content="VSCO">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.vsco} target="_blank" rel="noreferrer noopener" aria-label="VSCO Link">
+                        <VSCOIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.steam} target="_blank" rel="noreferrer noopener" aria-label="Steam Link">
-                    <SteamIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li>
+            <Tooltip content="Steam">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.steam} target="_blank" rel="noreferrer noopener" aria-label="Steam Link">
+                        <SteamIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.twitch} target="_blank" rel="noreferrer noopener" className="text-twitch" aria-label="Twitch Link">
-                    <TwitchIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li>
+            <Tooltip content="Twitch">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.twitch} target="_blank" rel="noreferrer noopener" className="text-twitch" aria-label="Twitch Link">
+                        <TwitchIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <a href={data.spotify} target="_blank" rel="noreferrer noopener" className="text-spotify" aria-label="Spotify Link">
-                    <SpotifyIcon width={28} height={28} fill="currentColor" />
-                </a>
-            </motion.li>
+            <Tooltip content="Spotify">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <a href={data.spotify} target="_blank" rel="noreferrer noopener" className="text-spotify" aria-label="Spotify Link">
+                        <SpotifyIcon width={28} height={28} fill="currentColor" />
+                    </a>
+                </motion.li>
+            </Tooltip>
 
-            <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
-                <button onClick={copyDiscord} className="text-discord flex items-center justify-center" aria-label="Copy Discord Tag">
-                    <DiscordIcon width={36} height={36} fill="currentColor" />
-                </button>
-            </motion.li>
+            <Tooltip content="Discord">
+                <motion.li variants={item} whileHover={{ opacity: 0.5, scale: 0.98 }} whileTap={{ opacity: 0.5, scale: 0.98 }}>
+                    <button onClick={copyDiscord} className="text-discord flex items-center justify-center" aria-label="Copy Discord Tag">
+                        <DiscordIcon width={36} height={36} fill="currentColor" />
+                    </button>
+                </motion.li>
+            </Tooltip>
         </motion.ul>
     );
 }
