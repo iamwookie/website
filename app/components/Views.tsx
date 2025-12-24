3d9cf8392f27e54d9d62cb72629aea4ac1da4801
Bilal <mail@bil.al>
2025-12-24 18:59:13 +0000

    fix(views): make filtering actually work

--- app/components/Views.tsx
+++ app/components/Views.tsx
@@ -5,33 +5,32 @@ import EyeIcon from '@public/assets/ui/eye-regular.svg';
 
 const crawlers = ['bot', 'crawl', 'spider', 'vercel', 'google', 'facebook', 'slurp', 'bing', 'discord', 'slack'];
 
-async function fetchViews(id: string, ff: string | null, ua: string | null): Promise<number> {
+async function fetchViews(id: string, ff: string | null, rip: string | null, ua: string | null): Promise<number> {
     const key = `views:${id}`;
 
-    if (!ff || !ua) return (await redis.get<number>(key).catch(() => 0)) ?? 0;
+    const fetchCount = async () => (await redis.get<number>(key).catch(() => 0)) ?? 0;
 
-    const ip = ff.split(',')[0].trim() || 'unknown';
-    const bot = crawlers.some((crawler) => ua.toLowerCase().includes(crawler));
+    if (!ff && !rip && !ua) return await fetchCount();
 
-    if (ip == 'unknown' || bot) return (await redis.get<number>(key).catch(() => 0)) ?? 0;
+    const ip = ff?.split(',')[0].trim() || rip || 'unknown';
+    const bot = crawlers.some((crawler) => ua?.toLowerCase().includes(crawler));
+    if (ip == 'unknown' || bot) return await fetchCount();
 
     const query = `${key}:${ip}`;
-    const viewed = await redis.get(query).catch(() => null);
-
-    if (viewed) return (await redis.get<number>(key).catch(() => 0)) ?? 0;
-
-    await redis.set(query, '1', { ex: 60 * 60 * 24 });
+    const set = await redis.set(query, '1', { ex: 60 * 60 * 24, nx: true });
+    if (!set) return await fetchCount();
 
     return (await redis.incr(key).catch(() => 0)) ?? 0;
 }
 
 export default async function Views({ id }: { id: string }) {
-    const reqHeaders = await headers();
+    const headersList = await headers();
 
-    const ff = reqHeaders.get('x-forwarded-for');
-    const ua = reqHeaders.get('user-agent');
+    const ff = headersList.get('x-forwarded-for');
+    const rip = headersList.get('x-real-ip');
+    const ua = headersList.get('user-agent');
 
-    const views = await fetchViews(id, ff, ua);
+    const views = await fetchViews(id, ff, rip, ua);
 
     return (
         <span className="flex flex-row items-center justify-center gap-2">
