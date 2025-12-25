238d1b591c485a765c4209888a12de1a6f876626
Bilal <mail@bil.al>
2025-12-25 11:04:40 +0000

    style(thoughts): add description

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -16,6 +16,10 @@ export default function Thoughts() {
         <main className="flex flex-col">
             <section className="container mx-auto flex min-h-dvh flex-col items-center justify-center gap-6 px-4">
                 <h1 className="text-xl text-white sm:text-2xl md:text-3xl">Share a thought</h1>
+                <h3 className="md:text-md max-w-md text-center text-xs text-white/70 sm:text-sm">
+                    A thought is something that lingers in your mind. It could be a quote, an idea, a feeling, or anything worth sharing. A
+                    thought is related to you, not me. Be thoughtful.
+                </h3>
                 <ThoughtForm />
             </section>
         </main>
