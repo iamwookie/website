8830c57dcd3bd37f71489bbf9a346aa395925ac1
Bilal <mail@bil.al>
2026-03-21 19:44:52 +0000

    chore: remove eid

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,9 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-
-import EidTitle from './EidTitle';
 import Title from './Title';
 
 export default function Header() {
-    const pathname = usePathname();
-
     return (
         <header className="fixed z-10 flex w-full items-center justify-center py-4">
-            {pathname.startsWith('/eid') ? <EidTitle /> : <Title />}
+            <Title />
         </header>
     );
 }
