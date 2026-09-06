af63b87f7c2e551473725c29a784fbe087ed5b9a
Bilal <mail@bil.al>
2026-09-06 21:47:10 +0100

    fix: views layout shift

--- app/components/views/Loading.tsx
+++ app/components/views/Loading.tsx
@@ -2,7 +2,7 @@ import EyeIcon from '@public/assets/ui/eye-regular.svg';
 
 export default function Loading() {
     return (
-        <span className="flex flex-row items-center justify-center gap-2">
+        <span className="flex h-6 flex-row items-center justify-center gap-2">
             <EyeIcon width={16} height={16} fill="currentColor" />
             <span className="h-4 w-8 animate-pulse rounded-full bg-white/10" />
         </span>
